15cf38011663df03d1a52c9fdbca7dff94b17cd1
Abboud21 <abboudmucy@gmail.com>
2025-09-23 11:46:18 -0300

    Initial commit

---
+++ src/main/java/br/com/dio/model/Money.java
@@ -0,0 +1,21 @@
+package br.com.dio.model;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+@EqualsAndHashCode
+@ToString
+@Getter
+public class Money {
+
+    private final List<MoneyAudit> history = new ArrayList<>();
+
+    public Money(final MoneyAudit history) {
+        this.history.add(history);
+    }
+
+    public void addHistory(final MoneyAudit history){
+        this.history.add(history);
+    }
+}
